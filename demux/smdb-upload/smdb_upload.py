bf32912e1b2b6a43e6941ff2e0a572beea91caca
Magnus Johannsen <48016378+magnujo@users.noreply.github.com>
2026-01-29 15:22:27 +0100

    Update SQL query table_id from 47 to 9

--- demux/smdb-upload/smdb_upload.py
+++ demux/smdb-upload/smdb_upload.py
@@ -87,7 +87,7 @@ def upload_demultiplex_stats(path_to_demultiplex_stats, path_to_run_info, path_t
     
     ENGINE = create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")    
     
-    q = 'select column_name_db, column_name_sheet from name_maps.column_names where table_id = 47;'
+    q = 'select column_name_db, column_name_sheet from name_maps.column_names where table_id = 9;'
 
     renamer = pd.read_sql(q, ENGINE)
     rename_dict = dict(zip(renamer['column_name_sheet'], renamer['column_name_db']))
